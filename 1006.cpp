5f0ebe28fd6d6a3e8a245a8c6c357a5cc02f3a4a
bobobobbbb <1105388289@qq.com>
2017-04-06 10:52:31 +0800

    提交

--- 1006.cpp
+++ 1006.cpp
@@ -1,4 +1,40 @@
-//部分A+B
+//1016.部分A+B
 //4月6日
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+long getPi(const string &, char);
+
+int main () {
+    string A;
+    char DA;
+    cin >> A >> DA;
+    int PAi = getPi(A, DA);
+
+    string B;
+    char DB;
+    cin >> B >> DB;
+    int PBcount = 0;
+    int PBi = getPi(B, DB);
+
+    cout << PBi + PAi << endl;
+
+    return 0;
+}
+
+long getPi(const string & A, char DA) {
+    int PAcount = 0;
+    for (auto &i : A) {
+        if (i == DA) {
+            PAcount++;
+        }
+    }
+    string PA = "";
+    for (int i = 0; i < PAcount; i++) {
+        PA += DA;
+    }
+    long PAi = stoi(PA);
+    return PAi;
+}
