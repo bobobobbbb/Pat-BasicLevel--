68c617f84a39b721042ad3e6e2510e31c252daed
bobobobbbb <1105388289@qq.com>
2017-04-24 21:07:12 +0800

    加油刷pat，尽早进入下一个阶段

---
+++ 1009.py
@@ -0,0 +1,40 @@
+#python 3
+#Pat乙级真题 
+#1009.1019数字黑洞
+
+def getProgressiveNumberStr(a_str):
+    rel_str = ''
+    a_lst = list(a_str)
+    while a_lst:
+        a = max(a_lst)
+        rel_str += a
+        a_lst.pop(a_lst.index(a))
+    return rel_str
+
+def getDecreaseNumberStr(a_str):
+    rel_str = ''
+    a_lst = list(a_str)
+    while a_lst:
+        a = min(a_lst)
+        rel_str += a
+        a_lst.pop(a_lst.index(a))
+    return rel_str
+
+def getResultStr(Inc_str, Dec_str):
+    result_int = int(Inc_str) - int(Dec_str)
+    result_str = str(result_int)
+    while len(result_str) < 4:
+        result_str = '0' + result_str
+    return result_str
+
+InputNum = input()
+increase_str = getProgressiveNumberStr(InputNum)
+decrease_str = getDecreaseNumberStr(InputNum)
+while InputNum != '6174':
+    result_str = getResultStr(increase_str, decrease_str)
+    print(increase_str, ' - ', decrease_str, ' = ', result_str)
+    increase_str = getProgressiveNumberStr(result_str)
+    decrease_str = getDecreaseNumberStr(result_str)
+    if result_str == '6174':
+        break
+
