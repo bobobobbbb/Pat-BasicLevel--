98ac6fcc2994093856018a2f60885ef229aaa214
bobobobbbb <1105388289@qq.com>
2017-04-25 23:53:48 +0800

    刷啊

--- 1014.py
+++ 1014.py
@@ -1,1 +1,10 @@
-#
+#python3
+
+s = input()
+a = float(s)
+index = s.find('E')
+E = int(s[index + 1:])
+zeroCount = 0
+precise = index - E - 3
+outputPat = '%' + '.' + str(precise) + 'f'
+print(outputPat % a)
